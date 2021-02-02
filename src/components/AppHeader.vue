d52b63f6dae1059928013292e884a1e39a641707
promahmudul <mahmudul.coder@gmail.com>
2021-02-02 22:06:17 +0600

    file move to pages

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,8 +2,9 @@
   <nav
     class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"
   >
-    <router-link to="/dc-heros">Dc Heroes</router-link>
-    <router-link to="/calender">Calender</router-link>
+    <router-link class="mx-2" to="/">Home</router-link>
+    <router-link class="mx-2" to="/dc-heros">Dc Heroes</router-link>
+    <router-link class="mx-2" to="/calender">Calender</router-link>
   </nav>
 </template>
 <script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+     <div class="m-auto">
+          <h1 class="text-center my-5 font-bold">Welcome to Vue3 App</h1>
+     </div>
+</template>
